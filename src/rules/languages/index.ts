1c3b4cf0c27b6dfce0bd83b79fe59a5ac62cea19
Harpalsinh Sindhav <88263558+harpalll@users.noreply.github.com>
2026-01-24 15:04:27 +0530

    feat: add support for Laravel detection, add rules for php and laravel

--- src/rules/languages/index.ts
+++ src/rules/languages/index.ts
@@ -1,3 +1,4 @@
 export { typescriptRules, javascriptRules } from './typescript.js';
 export { pythonRules, fastapiRules, djangoRules } from './python.js';
 export { goRules, rustRules } from './go-rust.js';
+export { phpRules } from './php.js';
